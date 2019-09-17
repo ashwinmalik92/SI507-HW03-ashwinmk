96d1e56172e2d20eca8652275ef4cd6705860c1d
Ashwin Malik <ashwinmalik92@gmail.com>
2019-09-17 16:28:09 -0400

    added check_question

--- magic_eight.py
+++ magic_eight.py
@@ -4,3 +4,20 @@
 def get_question():
     question = input('What is your question? ')
     return(question)
+
+# checks if user asked a question
+# param: question
+# returns: none
+def check_question(question):
+    if question[-1] == "?":
+        provide_answer()
+    elif question == 'quit':
+        pass
+    else:
+        print('I\'m sorry, I can only answer questions.')
+
+# main program
+question = ''
+while question != 'quit':
+    question = get_question()
+    check_question(question)
