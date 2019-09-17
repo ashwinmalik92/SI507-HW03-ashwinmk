67d83edbeaae32c069d5a29d22a3d8c8ecc54924
Abraham Rycus <arycus@umich.edu>
2019-09-17 16:20:17 -0400

    Added answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,6 +1,15 @@
+import random
+
 # asks user to submit their question
 # param: none
 # returns: user question
 def get_question():
     question = input('What is your question? ')
     return(question)
+
+answers = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes- definitely", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you know.", "Cannot predict now.", "Concentrate and ask again.","Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.","Very doubtful."]
+
+def provide_answer():
+    index = random.randint(0,19)
+    return answers[index]
+    
