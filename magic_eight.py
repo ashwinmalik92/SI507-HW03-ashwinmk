42f8636f2e531457a7fd0c4d7e0a56b08296ce63
Ashwin Malik <ashwinmalik92@gmail.com>
2019-09-17 16:48:26 -0400

    Final comment touches

--- magic_eight.py
+++ magic_eight.py
@@ -7,9 +7,11 @@ def get_question():
     question = input('What is your question? ')
     return(question)
 
-answers = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes- definitely", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you know.", "Cannot predict now.", "Concentrate and ask again.","Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.","Very doubtful."]
-
+# provides a random answer
+# param: none
+# returns: answer
 def provide_answer():
+    answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you know.", "Cannot predict now.", "Concentrate and ask again.","Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.","Very doubtful."]
     index = random.randint(0,19)
     return answers[index]
 
